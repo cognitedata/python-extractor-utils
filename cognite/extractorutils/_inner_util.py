fb59533e6ae126914d4d8bec6052ccd90f34308d
Mathias Lohne <mathias.lohne@cognite.com>
2019-11-19 14:38:51 +0100

    move out log level resolving

--- cognite/extractorutils/_inner_util.py
+++ cognite/extractorutils/_inner_util.py
@@ -59,3 +59,7 @@ class _EitherId:
 
     def __repr__(self) -> str:
         return self.__str__()
+
+
+def _resolve_log_level(level: str) -> int:
+    return {"NOTSET": 0, "DEBUG": 10, "INFO": 20, "WARNING": 30, "ERROR": 40, "CRITICAL": 50}[level.upper()]
