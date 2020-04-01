fc6747888b736236dabccd6bfb67fdee3463e37e
Mathias Lohne <mathias.lohne@cognite.com>
2020-04-01 14:26:36 +0200

    Prepare for open sourcing (#39)
     * Choosing Apache v2 as license
     * Add license keyword to pyproject.toml
     * Add copyright and license notice to source files
     * Add contribution section to README, including CoC and pre-commit tip
     * Use official repos for pre-commit hooks

--- cognite/extractorutils/statestore.py
+++ cognite/extractorutils/statestore.py
@@ -1,3 +1,17 @@
+#  Copyright 2020 Cognite AS
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 """
 The ``statestore`` module contains classes for keeping track of the extraction state of individual items, facilitating
 incremental load and speeding up startup times.
@@ -6,6 +20,7 @@ At the beginning of a run the extractor typically calls the ``initialize`` metho
 remote store (which can either be a local JSON file or a table in CDF RAW), and during and/or at the end of a run, the
 ``synchronize`` method is called, which saves the current states to the remote store.
 """
+
 import json
 from abc import ABC, abstractmethod
 from threading import Lock
