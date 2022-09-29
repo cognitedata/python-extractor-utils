ec589d3654bff0d7d946c50358cb1cf34366820e
Timur Samkharadze <timur.samkharadze@cognitedata.com>
2022-09-29 09:58:37 +0200

    fixed formatting

--- tests/tests_unit/test_middleware.py
+++ tests/tests_unit/test_middleware.py
@@ -13,8 +13,9 @@
 #  limitations under the License.
 import unittest
 
-from cognite.extractorutils.middleware import JQMiddleware
 from cognite.client.data_classes import Row
+
+from cognite.extractorutils.middleware import JQMiddleware
 
 
 class TestMiddlewareClass(unittest.TestCase):
@@ -26,25 +27,16 @@ class TestMiddlewareClass(unittest.TestCase):
                 "new_col": "some value"
             }
         """
-    data = {
-            "_foo": "foo",
-            "_bar": "bar",
-            "_baz": "baz"
-        }
+    data = {"_foo": "foo", "_bar": "bar", "_baz": "baz"}
 
-    expected_result = {
-            "foo": "foo",
-            "bar": "bar",
-            "baz": "baz",
-            "new_col": "some value"
-        }
+    expected_result = {"foo": "foo", "bar": "bar", "baz": "baz", "new_col": "some value"}
 
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName)
         self.mw = JQMiddleware(jq_rules=self.jq_rules)
-    
+
     def test_jq_mw_dict(self):
-        
+
         actual_result = self.mw(self.data)
         self.assertDictEqual(actual_result, self.expected_result)
 
