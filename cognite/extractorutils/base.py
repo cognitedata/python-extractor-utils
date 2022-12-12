69db1eb0ea51f8e31003245166dffe93a63fa4ea
Pavel Zubarev <40171187+pavelzubarev@users.noreply.github.com>
2022-12-12 18:22:14 +0000

    Don't skip reading the configuration for Cognite Functions (#173)

--- cognite/extractorutils/base.py
+++ cognite/extractorutils/base.py
@@ -253,20 +253,20 @@ class Extractor(Generic[CustomConfigClass]):
             else:
                 dotenv_message = "No .env file found"
 
-            try:
-                self._initial_load_config(override_path=self.config_file_path)
-            except InvalidConfigError as e:
-                print("Critical error: Could not read config file", file=sys.stderr)
-                print(str(e), file=sys.stderr)
-                sys.exit(1)
+        try:
+            self._initial_load_config(override_path=self.config_file_path)
+        except InvalidConfigError as e:
+            print("Critical error: Could not read config file", file=sys.stderr)
+            print(str(e), file=sys.stderr)
+            sys.exit(1)
 
-            if not self.configured_logger:
-                self.config.logger.setup_logging()
-                self.configured_logger = True
+        if not self.configured_logger:
+            self.config.logger.setup_logging()
+            self.configured_logger = True
 
-            self.logger = logging.getLogger(__name__)
-            self.logger.info(dotenv_message)
-            self.logger.info(f"Loaded {'remote' if self.config_resolver.is_remote else 'local'} config file")
+        self.logger = logging.getLogger(__name__)
+        self.logger.info(dotenv_message)
+        self.logger.info(f"Loaded {'remote' if self.config_resolver.is_remote else 'local'} config file")
 
         if self.handle_interrupts:
             set_event_on_interrupt(self.cancelation_token)
