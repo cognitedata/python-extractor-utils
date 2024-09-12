c6980de3651640cb4f8fe9b7d9af81cfdf25ff5e
Mathias Lohne <mathias.lohne@cognite.com>
2024-09-12 13:33:52 +0200

    Workaround for 3.10

--- cognite/extractorutils/unstable/core/runtime.py
+++ cognite/extractorutils/unstable/core/runtime.py
@@ -5,7 +5,13 @@ import time
 from argparse import ArgumentParser, Namespace
 from multiprocessing import Process, Queue
 from pathlib import Path
-from typing import Any, Generic, Type, TypeVar, assert_never
+from typing import Any, Generic, Type, TypeVar
+
+try:
+    from typing import assert_never
+except ImportError:
+    # workaround for 3.10
+    from typing_extensions import assert_never
 
 from cognite.extractorutils.threading import CancellationToken
 from cognite.extractorutils.unstable.configuration.loaders import load_file, load_from_cdf
