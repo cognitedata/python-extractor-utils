88f370e190ecad3a8bdc3bc769f6a79443d14bbe
Mathias Lohne <mathias.lohne@cognite.com>
2024-11-26 11:01:47 +0100

    Add error reporting to integrations POC (#396)
    Add an `error` method to the `Extractor` class that starts tracking of
    an error. This can be used in one of two ways, either by manually
    starting and stopping the error state:
    
    ``` python
    e = extractor.error(...)
    
     # handle error
    
    e.finish()
    ```
    
    or by using it as a context
    
    ``` python
    with extractor.error(...):
        # Handle error
    ```
    
    You can create an instant error (with no duration) by using the
    `instant()` method:
    
    ``` python
    extractor.error(...).instant()
    ```
    
    Tracking of start/end times, generating and keeping track of external
    IDs, reporting in checkins, and so on is all handled automatically.

--- cognite/extractorutils/unstable/core/base.py
+++ cognite/extractorutils/unstable/core/base.py
@@ -1,7 +1,9 @@
 import logging
 from concurrent.futures import ThreadPoolExecutor
+from contextvars import ContextVar, Token
 from multiprocessing import Queue
 from threading import RLock, Thread
+from traceback import format_exception
 from types import TracebackType
 from typing import Generic, Literal, Optional, Type, TypeVar, Union
 
@@ -10,8 +12,10 @@ from typing_extensions import Self, assert_never
 
 from cognite.extractorutils.threading import CancellationToken
 from cognite.extractorutils.unstable.configuration.models import ConnectionConfig, ExtractorConfig
+from cognite.extractorutils.unstable.core._dto import Error as DtoError
 from cognite.extractorutils.unstable.core._dto import TaskUpdate
 from cognite.extractorutils.unstable.core._messaging import RuntimeMessage
+from cognite.extractorutils.unstable.core.errors import Error, ErrorLevel
 from cognite.extractorutils.unstable.core.tasks import ContinuousTask, ScheduledTask, StartupTask, Task
 from cognite.extractorutils.unstable.scheduling import TaskScheduler
 from cognite.extractorutils.util import now
@@ -50,8 +54,11 @@ class Extractor(Generic[ConfigType]):
 
         self._tasks: list[Task] = []
         self._task_updates: list[TaskUpdate] = []
+        self._errors: dict[str, Error] = {}
 
         self.logger = logging.getLogger(f"{self.EXTERNAL_ID}.main")
+
+        self._current_task: ContextVar[str | None] = ContextVar("current_task", default=None)
 
     def _set_runtime_message_queue(self, queue: Queue) -> None:
         self._runtime_messages = queue
@@ -61,11 +68,26 @@ class Extractor(Generic[ConfigType]):
             task_updates = [t.model_dump() for t in self._task_updates]
             self._task_updates.clear()
 
+            error_updates = [
+                DtoError(
+                    external_id=e.external_id,
+                    level=e.level.value,
+                    description=e.description,
+                    details=e.details,
+                    start_time=e.start_time,
+                    end_time=e.end_time,
+                    task=e._task_name if e._task_name is not None else None,
+                ).model_dump()
+                for e in self._errors.values()
+            ]
+            self._errors.clear()
+
         res = self.cognite_client.post(
             f"/api/v1/projects/{self.cognite_client.config.project}/odin/checkin",
             json={
                 "externalId": self.connection_config.extraction_pipeline,
                 "taskEvents": task_updates,
+                "errors": error_updates,
             },
             headers={"cdf-version": "alpha"},
         )
@@ -83,13 +105,35 @@ class Extractor(Generic[ConfigType]):
                 self.logger.exception("Error during checkin")
             self.cancellation_token.wait(10)
 
+    def _report_error(self, error: Error) -> None:
+        with self._checkin_lock:
+            self._errors[error.external_id] = error
+
+    def error(
+        self,
+        level: ErrorLevel,
+        description: str,
+        details: str | None = None,
+        *,
+        force_global: bool = False,
+    ) -> Error:
+        task_name = self._current_task.get()
+
+        return Error(
+            level=level,
+            description=description,
+            details=details,
+            extractor=self,
+            task_name=None if force_global else task_name,
+        )
+
     def restart(self) -> None:
         if self._runtime_messages:
             self._runtime_messages.put(RuntimeMessage.RESTART)
         self.cancellation_token.cancel()
 
     @classmethod
-    def init_from_runtime(
+    def _init_from_runtime(
         cls,
         connection_config: ConnectionConfig,
         application_config: ConfigType,
@@ -98,18 +142,44 @@ class Extractor(Generic[ConfigType]):
         return cls(connection_config, application_config, current_config_revision)
 
     def add_task(self, task: Task) -> None:
+        # Store this for later, since we'll override it with the wrapped version
         target = task.target
 
         def wrapped() -> None:
+            """
+            A wrapped version of the task's target, with tracking and error handling
+            """
+            # Record a task start
             with self._checkin_lock:
                 self._task_updates.append(
                     TaskUpdate(type="started", name=task.name, timestamp=now()),
                 )
 
+            context_token: Token[str | None] | None = None
+
             try:
+                # Set the current task context var, used to track that we're in a task for error reporting
+                context_token = self._current_task.set(task.name)
+
+                # Run task
                 target()
 
+            except Exception as e:
+                # Task crashed, record it as a fatal error
+                self.error(
+                    ErrorLevel.fatal,
+                    description="Task crashed unexpectedly",
+                    details="".join(format_exception(e)),
+                ).instant()
+
+                raise e
+
             finally:
+                # Unset the current task
+                if context_token is not None:
+                    self._current_task.reset(context_token)
+
+                # Record task end
                 with self._checkin_lock:
                     self._task_updates.append(
                         TaskUpdate(type="ended", name=task.name, timestamp=now()),
@@ -186,7 +256,7 @@ class Extractor(Generic[ConfigType]):
                 case _:
                     assert_never(task)
 
-        self.logger.info("Starting up extractor")
+        self.logger.info("Starting extractor")
         if startup:
             with ThreadPoolExecutor() as pool:
                 for task in startup:
