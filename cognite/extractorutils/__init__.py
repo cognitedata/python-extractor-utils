6dc6748224e376115a1a5a1cc28b4ca984f649af
Nithin Bodanapu <nithin.bodanapu@cognite.com>
2025-02-07 10:15:01 +0530

    Changes suggested in the PR related to the version and the changelog phrasing

--- cognite/extractorutils/__init__.py
+++ cognite/extractorutils/__init__.py
@@ -16,7 +16,7 @@
 Cognite extractor utils is a Python package that simplifies the development of new extractors.
 """
 
-__version__ = "7.6.0"
+__version__ = "7.5.12"
 from .base import Extractor
 
 __all__ = ["Extractor"]
