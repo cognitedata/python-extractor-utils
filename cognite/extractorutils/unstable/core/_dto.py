d5684dd396cfd3689f7495f34b655a75056e62f5
Babatunde Aromire <babatunde.aromire@cognite.com>
2025-07-18 14:24:05 +0200

    Restructring some dto objects

--- cognite/extractorutils/unstable/core/_dto.py
+++ cognite/extractorutils/unstable/core/_dto.py
@@ -2,10 +2,12 @@
 Temporary holding place for DTOs against Extraction Pipelines 2.0 until it's in the SDK.
 """
 
-from typing import Any, Literal
+from enum import Enum
+from typing import Annotated, Any, Literal, Optional, TypeAliasType
 
+from annotated_types import Len
 from humps import camelize
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, StringConstraints
 
 
 class CogniteModel(BaseModel):
@@ -30,13 +32,17 @@ class CogniteModel(BaseModel):
     model_config = ConfigDict(alias_generator=camelize, populate_by_name=True, extra="forbid")
 
 
+class HasExternalId(CogniteModel):
+    external_id: str
+
+
 class TaskUpdate(CogniteModel):
     type: Literal["started"] | Literal["ended"]
     name: str
     timestamp: int
 
 
-class Error(CogniteModel):
+class Error(HasExternalId):
     external_id: str
     level: str
     description: str
@@ -44,3 +50,56 @@ class Error(CogniteModel):
     start_time: int
     end_time: int | None
     task: str | None
+
+
+TaskUpdateList = Annotated[list[TaskUpdate], Len(min_length=1, max_length=1000)]
+ErrorList = Annotated[list[Error], Len(min_length=0, max_length=1000)]
+VersionType = Annotated[str, StringConstraints(min_length=1, max_length=32)]
+DescriptionType = Annotated[str, StringConstraints(min_length=0, max_length=500)]
+TaskList = Annotated[list["Task"], Len(min_length=1, max_length=1000)]
+JSONType = TypeAliasType(  # type: ignore
+    "JSONType",
+    bool
+    | int
+    | float
+    | str
+    | None
+    | list[Optional["JSONType"]]  # type: ignore
+    | dict[str, Optional["JSONType"]],  # type: ignore  # type: ignore
+)
+
+
+class HasVersion(CogniteModel):
+    version: VersionType | None = None
+
+
+class ExtractorInfo(HasExternalId, HasVersion):
+    pass
+
+
+class TaskType(Enum):
+    continuous = "continuous"
+    batch = "batch"
+
+
+class Task(CogniteModel):
+    type: TaskType
+    name: str
+    action: bool = False
+    description: DescriptionType | None = None
+
+
+class StartupRequest(HasExternalId):
+    extractor: ExtractorInfo
+    tasks: TaskList | None = None
+    active_config_revision: int | Literal["local"] | None = None
+    timestamp: int | None = None
+
+
+class CheckinRequest(HasExternalId):
+    task_events: TaskUpdateList | None = None
+    errors: ErrorList | None = None
+
+
+class CheckinResponse(HasExternalId):
+    last_config_revision: int | None
