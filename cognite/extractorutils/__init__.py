ba51afec9eaa51f34330ef58aa00e60a1ba83868
Hemant Mishra <hemant.mishra@cognite.com>
2026-03-23 14:07:59 +0530

    Release version

--- cognite/extractorutils/__init__.py
+++ cognite/extractorutils/__init__.py
@@ -16,7 +16,7 @@
 Cognite extractor utils is a Python package that simplifies the development of new extractors.
 """
 
-__version__ = "7.11.6"
+__version__ = "7.12.0"
 from .base import Extractor
 
 __all__ = ["Extractor"]
